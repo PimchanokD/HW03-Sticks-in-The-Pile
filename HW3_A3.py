1e4976530510485631ad66d348d80e8781bb66c3
DESKTOP-72LTF07\User <nui.pimcha@gmail.com>
2021-07-27 13:18:25 +0700

    add loop

--- HW3_A3.py
+++ HW3_A3.py
@@ -12,7 +12,7 @@ i = 1
 player1 = True
 player2 = True
 while total_pile > 0:
-    if player1 == True:
+    if player1:
         if total_pile == 1:
             pick = 1
             print('I, smart computer, takes the last stick.')
@@ -20,7 +20,7 @@ while total_pile > 0:
         elif total_pile%3 == 0:
             pick = 2
             print('I take %d stick, there are %d sticks in the pile.' %(pick,total_pile-pick))
-        elif total_pile == 2:
+        elif total_pile == 2 or 5:
             pick = 1
             print('I take %d stick, there are %d sticks in the pile.' %(pick,total_pile-pick))
         else:
@@ -29,24 +29,34 @@ while total_pile > 0:
             print('I take %d stick, there are %d sticks in the pile.' %(pick,total_pile-pick))
         total_pile = total_pile - pick
     player1 = not player1
-        
-    if player2 == True:
-        pick = int(input('How many you will take (1 or 2): '))
-        if 0 < pick <= 2:
-            if total_pile - pick < 0:
-                print('There are no enough sticks to take')
-            else:
-                if total_pile == 1:
-                    print('You take the last stick,')
-                    break
+    
+    if player2:
+        j = 1
+        while player2 == True:
+            pick = int(input('How many you will take (1 or 2): '))
+            if 0 < pick <= 2:
+                if total_pile - pick < 0:
+                    print('There are no enough sticks to take')
+                    player2 = True
                 else:
-                    print('There are %d sticks in the pile' %(total_pile-pick))
-                    print()
-                total_pile = total_pile - pick
-        elif pick > 2:
-            print('No you cannot take more than 2 sticks!')
-        elif pick < 1:
-            print('No you cannot take more less than 1 sticks!')
+                    player2 = not player2
+            elif pick > 2:
+                print('No you cannot take more than 2 sticks!')
+                player2 = True
+            elif pick < 1:
+                print('No you cannot take more less than 1 sticks!')
+                player2 = True
+        j = j + 1
+        
+        if total_pile > 0:
+            if total_pile == 1:
+                print('You take the last stick,')
+                break
+            else:
+                print('There are %d sticks in the pile' %(total_pile-pick))
+                print()
+                player2 = not player2
+            total_pile = total_pile - pick
     player2 = not player2
     i = i + 1
 
